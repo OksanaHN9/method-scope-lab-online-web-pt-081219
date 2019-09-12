35f27cba0ff30dd58b773be6b75ce4054f2b1b3c
Oksana Nichols <oksanahnichols@yahoo.com>
2019-09-12 03:07:28 +0000

    Automatically backed up by Learn

--- lib/catch_phrase.rb
+++ lib/catch_phrase.rb
@@ -1,5 +1,10 @@
+<<<<<<< HEAD
 def catch_phrase
   puts phrase = "It's-a me, Mario"
   expect{catch_phrase}.to output("It's-a me, Mario"\n").to_stdout
   end
+=======
+def catch_phrase(phrase)
+  puts phrase
+>>>>>>> 145abc5aa3d4a87945780ef963c2ea3fe40018ee
 end
